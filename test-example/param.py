f05f25a7239e2ee7ce5182ddfd34977002a7a2b5
bockelmann <bockelmann@rob.uni-luebeck.de>
2023-04-02 22:31:02 +0200

    Add smoke and unit test

---
+++ test-example/param.py
@@ -0,0 +1,18 @@
+WANDB_PROJECT = "mlops-course-001"
+ENTITY = None
+BDD_CLASSES = {
+    i: c
+    for i, c in enumerate(
+        [
+            "background",
+            "road",
+            "traffic light",
+            "traffic sign",
+            "person",
+            "vehicle",
+            "bicycle",
+        ]
+    )
+}
+RAW_DATA_AT = "av-team/mlops-course-001/bdd_simple_1k"
+PROCESSED_DATA_AT = "av-team/mlops-course-001/bdd_simple_1k_split"
